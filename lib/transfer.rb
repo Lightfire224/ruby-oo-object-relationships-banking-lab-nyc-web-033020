b1a9815d35e56438e48272c642296d8552384ac4
lightfire224 <elias.taveras@columbia.edu>
2020-04-02 16:33:46 -0400

    message

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+    attr_reader :sender, :receiver, :balance, :status
+
+  def initialize(sender, receiver, balance)
+    @sender = sender
+    @receiver = receiver
+    @balance = balance
+    @status = "pending"
+    
+  end
+
 end
