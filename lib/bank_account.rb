cf51a1e73d5119a5a538b78657bc772327499d46
lightfire224 <elias.taveras@columbia.edu>
2020-04-02 21:31:03 -0400

    use a useful message here like what you did

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,18 @@
 class BankAccount
     attr_reader :name
-    attr_accessor :balance, :status
+    attr_accessor :balance, :status, :withdraw
     def initialize(name)
         @name = name
         @balance = 1000
         @status = "open"
     end
 
-    def deposit(balance)
-        @balance  += self.balance
+    def deposit(amount)
+        @balance  += amount
+    end
+
+    def withdraw(amount)
+        @balance -= amount
     end
 
     def display_balance
@@ -16,11 +20,7 @@ class BankAccount
     end
 
     def valid?
-        if @status == "open" && @balance > 0
-            true
-        else
-            false
-        end
+        @status == "open" && @balance > 0
     end
 
     def close_account
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,46 @@
 class Transfer
-    attr_reader :sender, :receiver, :balance, :status
+    attr_reader :sender, :receiver, :balance, :status, :amount
 
-  def initialize(sender, receiver, balance)
+  # BankAccount sender, BankAccount receiver, Integer amount
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @balance = balance
     @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    rejection_message = "Transaction rejected. Please check your account balance."
     
+    if receiver.status == "closed"
+      @status = "rejected"
+      return rejection_message
+    elsif sender.balance < amount
+      @status = "rejected"
+      return rejection_message
+    end
+
+    if @status == "pending"
+      receiver.deposit(amount)
+      sender.withdraw(amount)
+      #Could have deposited a negative amount instead of making withdraw method
+      @status = "complete"
+    end
+  end
+  
+  def reverse_transfer
+    #withdraw amount from receivers balance
+    #deposit amount from senders balance 
+    #@status = "reversed"
+    if @status == "complete"
+      receiver.withdraw(amount)
+      sender.deposit(amount)
+      @status = "reversed"
+    end
   end
 
 end
